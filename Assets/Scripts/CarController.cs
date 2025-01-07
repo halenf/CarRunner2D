600bd84ff5203cf5b1427fe00e3bdc775a8221c0
Halen Finlay <halenfinlay@gmail.com>
2025-01-07 22:12:16 +1030

    Initial work commit
    Has the basic stuff I made before creating the repo.

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,19 @@
+///<summary>
+/// Author: Halen
+///
+/// make car go yay
+///
+///</summary>
+
+using UnityEngine;
+
+namespace CarRunner2D
+{
+	public class CarController : MonoBehaviour
+	{
+		public void GetMoveInput(InputAction.CallbackContext context)
+        {
+
+        }
+	}
+}
