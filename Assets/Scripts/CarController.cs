7fa9c4af28aefa88a2dd579c66c6482e3b921cef
Halen Finlay <halenfinlay@gmail.com>
2025-01-07 22:13:58 +1030

    quick notes for next time

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,14 +6,22 @@
 ///</summary>
 
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace CarRunner2D
 {
 	public class CarController : MonoBehaviour
 	{
+		// wheel joint references
+		
 		public void GetMoveInput(InputAction.CallbackContext context)
         {
 
         }
+		
+		public void Update()
+		{
+			// add torque to wheels
+		}
 	}
 }
